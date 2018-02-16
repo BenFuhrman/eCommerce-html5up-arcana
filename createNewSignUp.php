5291c805989b7204539a7bf3b4d61c479436f373
Jake Wilson <jaw4fz@virginia.edu>
2018-02-15 19:39:09 -0500

    lots of changes

--- createNewSignUp.php
+++ createNewSignUp.php
@@ -1,19 +1,27 @@
 <?php
 
-require 'connection.php';
-$conn    = Connect();
-$fname   = $_POST['name'];
-/*
-$lname   = $conn->real_escape_string($_POST['LastName']);
-$email    = $conn->real_escape_string($_POST['Email']);
-$password   = $conn->real_escape_string($_POST['Password']);
-$address   = $conn->real_escape_string($_POST['Address']);
-$city    = $conn->real_escape_string($_POST['City']);
-$state    = $conn->real_escape_string($_POST['State']);
-$zip   = $conn->real_escape_string($_POST['Zip']);
-$query   = "INSERT into customerdata (FirstName, LastName, Email, Password, Address, City, State, Zip) VALUES('" . $fname . "','" . $lname ."','" . $email . "','" . $password . "','" . $address . "','" . $city . "','" . $state . "','" . $zip . "')";
-*/
-$query = "INSERT into temptable (name) VALUES ('" . $fname . "')";
+include ("connection.php");
+$conn = Connect();
+if (isset($_POST['FirstName'])){
+	$fname = $_POST['FirstName'];
+}
+if (isset($_POST['LastName'])){
+	$lname   = $_POST['LastName'];
+}
+
+$email    = $_POST['Email'];
+$password   = $_POST['Password'];
+$address   = $_POST['Address'];
+$city    = $_POST['City'];
+$state    = $_POST['State'];
+$zip   = $_POST['Zip'];
+//$test1 = 'balogna';
+$sql = "INSERT INTO customerdata (FirstName, LastName, Email, Password, Address, City, State, Zip) VALUES ($fname, $lname, $email, $password, $address, $city, $state, $zip)";
+$result = $conn->query($sql);
+//VALUES ($fname, $lname, $email, $password, $address, $city, $state, $zip)";
+//$query   = "INSERT into customerdata (FirstName, LastName, Email, Password, Address, City, State, Zip) VALUES('" . $fname . "','" . $lname ."','" . $email . "','" . $password . "','" . $address . "','" . $city . "','" . $state . "','" . $zip . "')";
+
+//$query = "INSERT into temptable (name) VALUES ('" . $fname . "')";
 
  
 echo "Thank You For Contacting Us <br>";
