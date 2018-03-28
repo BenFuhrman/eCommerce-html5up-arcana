d50447f749c877dc0ef2111f043efde69bb68edd
Jake Wilson <jaw4fz@virginia.edu>
2018-03-28 17:31:42 -0400

    login

--- login.php
+++ login.php
@@ -8,7 +8,14 @@ $toinsertpass =  $_POST["Password"];
 $hash = password_hash($toinsertpass, PASSWORD_DEFAULT);
 
 $sqlcheckemail = "SELECT * FROM customerdata WHERE Email='$toinsertemail' AND Password='$hash'";
+$customername = "SELECT FirstName FROM customerdata WHERE Email='$toinsertemail'";
 $amountwithsameemail = $conn->query($sqlcheckemail);
 $numentries = mysqli_num_rows($amountwithsameemail);
-$isAllowed = $numentries > 0;
+$accountthere = $numentries > 0;
+if ($accountthere == true) {
+	$return = "<font color=#008000><Center><b>**Successful Login**</b></Center></font>";
+} else {
+	$return = "<font color=#ff0000><Center><b>**Failed Login**</b></Center></font>";
+}
+print($return);
 ?>
